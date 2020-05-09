04e1bd3d2afde886524d54c66dfdda624b5f4281
Hlaford347 <hlaford347@gmail.com>
2020-05-09 21:40:16 +0200

    Updates

---
+++ precache-manifest.2d60cd8fea16f04f489fd1a31305d128.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e361d0bc7635167796a99486603b0545",
+    "url": "/DnDCheatSheet/index.html"
+  },
+  {
+    "revision": "7aea38233d7aabe992f9",
+    "url": "/DnDCheatSheet/static/css/main.8ecb61c9.chunk.css"
+  },
+  {
+    "revision": "e24fde9a8b653afc9a5b",
+    "url": "/DnDCheatSheet/static/js/2.d6936090.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/DnDCheatSheet/static/js/2.d6936090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aea38233d7aabe992f9",
+    "url": "/DnDCheatSheet/static/js/main.e3eb2b03.chunk.js"
+  },
+  {
+    "revision": "4ed4ae86d723cee38738",
+    "url": "/DnDCheatSheet/static/js/runtime-main.3c686480.js"
+  }
+]);
