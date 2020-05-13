d7fb354cea4b5c62e07c825e2e1c5ece62a27a42
Hlaford347 <hlaford347@gmail.com>
2020-05-13 23:21:22 +0200

    Bug fix for heroku

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000; // Heroku will need the port env variable
 
-app.use('/static', express.static(path.join(__dirname, 'client/build')));
+app.use('/static', express.static(path.join(__dirname, 'build')));
 
 app.listen(port, () => console.log(`App is live on port ${port}`));
